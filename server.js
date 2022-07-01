ecf8846dee01767f8827cc9f6344074e8cc0ecd6
Terry Cheng <terryhycheng@gmail.com>
2022-07-01 15:07:49 +0800

    Local Sever Built, Clear Default Style

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
-
-const app = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
+const projectData = { id: 1, values: "Hello World!" };
+const port = 8000;
 
 // Require Express to run server and routes
+const express = require("express");
+const app = express();
 
 // Start up an instance of app
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -21,3 +22,16 @@ app.use(cors());
 app.use(express.static("website"));
 
 // Setup Server
+const server = app.listen(port, () => {
+  console.log(`running on loacalhost:${port}`);
+});
+
+/* RESTFUL API */
+
+//GET ROUTE
+app.get("/api", (req, res) => {
+  res.send(projectData);
+  console.log(projectData);
+});
+
+//POST ROUTE
